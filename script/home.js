138dbe5e78099762eb790e6738c7589b625c6879
JayRGopal <jay2003@me.com>
2023-01-13 14:52:54 -0500

    break statement in loop()

--- script/home.js
+++ script/home.js
@@ -174,6 +174,7 @@ function postVideo(video_link,ctx){
           }
           if(frame>((endLength*fps) - 1)){
             video.pause();
+            break;
           }
         })();
         image = $this;
