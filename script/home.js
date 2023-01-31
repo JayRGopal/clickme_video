182fde751b430dc8c3f2963554230c569fbedbb8
TimD123 <desimonetim@gmail.com>
2023-01-31 10:54:09 -0500

    a

--- script/home.js
+++ script/home.js
@@ -99,7 +99,7 @@ var color2 = "rgb("+r2+","+g2+","+b2+")";
 function getImage(ctx){
     // Chooses a random video to play
     // Calls on postVideo, sending the path to the video (JPEG files)
-    
+
     /*
     var jqxhr = $.get('/random_image', function () {
     }).done(function(data) {
@@ -208,6 +208,9 @@ function postVideo(video_link,ctx){
     
     final_image_path = master_path + intToTitle(frame) + ".jpg";
     image.src = 'data:image/JPG;base64,' + final_image_path;
+
+    image.src = "images/1_100.jpeg"
+    ctx.drawImage(image, sx=(this.width - im_crop_width)/2, sy=(this.height - im_crop_height)/2, sWidth=im_crop_width, sHeight=im_crop_height, dx=0, dy=0, dWidth=canvas_width, dHeight=canvas_height);
 
     (function loop() {
         if (frame<finalFrame) {
