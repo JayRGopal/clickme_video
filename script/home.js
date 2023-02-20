bd113690b6b7d7281b593558e1be9e54d67fd9f7
TimD123 <desimonetim@gmail.com>
2023-02-20 15:32:12 -0500

    a

--- script/home.js
+++ script/home.js
@@ -1107,7 +1107,7 @@ const getImagePath = () => {
     fetch('http://35.224.74.65:8090/imagePath', {
         method: 'post',
         headers: {'Content-Type': 'application/json'}
-    }).then(response => console.log(response.json()))
+    }).then(response => console.log(response))
 }
 /////////
 // device detection
